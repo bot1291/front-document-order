ffb5a5219370c4b0dce84ab2ada5a8b9f905b314
Igor Raskatov <aura.star@yandex.ru>
2022-12-05 15:31:31 +0300

    Repo create, add input-comp., add base layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+	Component,
+	pageProps,
+	router,
+}: AppProps): JSX.Element {
+	return (
+		<>
+			<Head>
+				<title>Заказ документа</title>
+				<meta name="description" content="Order document" />
+				<meta
+					property="og:url"
+					content={`${process.env.NEXT_PUBLIC_DOMAIN}${router.asPath}`}
+				/>
+				<meta property="og:locale" content="ru_RU" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+	render(): JSX.Element {
+		return (
+			<Html lang="ru">
+				<Head>
+					<link
+						rel="preconnect"
+						href="https://fonts.googleapis.com"
+					/>
+					<link
+						rel="preconnect"
+						href="https://fonts.googleapis.com"
+						crossOrigin=""
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+						rel="stylesheet"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
