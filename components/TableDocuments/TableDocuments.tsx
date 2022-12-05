eba5d31b268aa18e8009baff3e4b2f974ca13040
Igor Raskatov <aura.star@yandex.ru>
2022-12-05 22:31:40 +0300

    Fix form

--- components/TableDocuments/TableDocuments.tsx
+++ components/TableDocuments/TableDocuments.tsx
@@ -8,6 +8,18 @@ export const TableDocuments = ({
 }: TableDocumentsProps): JSX.Element => {
 	return (
 		<div className={`${className} ${styles.table}`} {...props}>
+			<div>
+				<div className={styles.documentBlock}>
+					<span className={styles.titleDocument}>
+						Наименование документа
+					</span>
+					<hr className={styles.hrVertical} />
+					<span className={styles.countDocument}>
+						Количество заявок
+					</span>
+				</div>
+				<hr className={styles.hrGorisontal} />
+			</div>
 			{allDocuments.map((d) => (
 				<div key={d.document}>
 					<div className={styles.documentBlock} key={d.document}>
