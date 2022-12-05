1e4701aef677c4a53b9356f8a07208787324b5d7
Igor Raskatov <aura.star@yandex.ru>
2022-12-05 22:18:31 +0300

    Small fix

--- components/TableDocuments/TableDocuments.tsx
+++ components/TableDocuments/TableDocuments.tsx
@@ -9,14 +9,14 @@ export const TableDocuments = ({
 	return (
 		<div className={`${className} ${styles.table}`} {...props}>
 			{allDocuments.map((d) => (
-				<>
+				<div key={d.document}>
 					<div className={styles.documentBlock} key={d.document}>
 						<span className={styles.title}>{d.document}</span>
 						<hr className={styles.hrVertical} />
 						<span>{d.count}</span>
 					</div>
 					<hr className={styles.hrGorisontal} />
-				</>
+				</div>
 			))}
 		</div>
 	);
